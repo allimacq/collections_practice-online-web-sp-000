32bb03342d9525ec494b5cced01fa8df248f357b
Allison MacQueen <allimacq@gmail.com>
2020-09-02 19:46:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,70 @@
+def sort_array_asc(integers)
+  integers.sort
+end
 
+def sort_array_desc(integers)
+  sorted = [ ]
+  sorted = integers.sort
+  sorted.reverse
+end
+
+def sort_array_char_count(string)
+  string.sort do |x, y|
+    x.length <=> y.length
+  end
+end
+
+def swap_elements(s)
+  s[1], s[2] = s[2], s[1]
+  s
+end
+
+def reverse_array(a)
+  a.reverse()
+end
+
+def kesha_maker(arr)
+  arr.collect do |name|
+    #breaking down each name by the characters it's made up of
+    break_down = name.chars
+    #going after the third character and reassigning it to the $
+    break_down[2] = "$"
+    #stitching the name back together with the join method.
+    new_name = break_down.join()
+    new_name
+  end
+end
+
+def find_a(word)
+  a_words = [ ]
+  word.collect do |x|
+    if x.start_with?("a") == true
+      #putting the words that start with a in a new array
+      a_words << x
+    end
+  end
+  #returning our new array
+  a_words
+end
+
+def sum_array(num)
+  num.sum
+end
+
+def add_s(text)
+  plurals = [ ]
+  i = 0
+  if i = 0
+    text[i] << "s"
+    i +=1
+    puts text
+  elsif i = 1
+    text[i] = text[1]
+    i += 1
+  elsif i > 1 && i <= text.length
+      text[i] << "s"
+    i +=1
+    puts text
+  end
+  #text
+end
